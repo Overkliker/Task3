e0db31e193e084f7defc8aa0a6b13cc707dc3ece
Danila <danilahreno@gmail.com>
2022-03-08 16:21:17 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/training/<list>')
+def nextlvl(list):
+    if 'ol' in list:
+        return render_template('list_prof_num.html')
+
+    elif 'ul' in list:
+        return render_template('list_prof_marks.html')
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
